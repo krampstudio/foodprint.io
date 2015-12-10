9aac7a554232ffe71e04464730ab06c372692eee
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-12-10 08:29:19 +0100

    node es2015 server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/hello',  (req, res) => res.send({ hello : 'wolrd'}));
+
+const server = app.listen(3000, 'localhost', () =>  {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
