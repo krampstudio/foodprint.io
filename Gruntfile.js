26c9ff32299cea3f1044bb36887c7e6c1c0949e6
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-01-25 08:21:13 +0100

    clean up Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,22 +4,8 @@ module.exports = function(grunt) {
 
         pkg: grunt.file.readJSON('package.json'),
 
-        connect: {
-            options : {
-                hostname: '<%=pkg.config.host%>',
-                port:     '<%=pkg.config.port%>',
-                base:     'public'
-            },
-            preview: {
-                options: {
-                    livereload : true
-                }
-            }
-        },
-
-
         mochaTest : {
-            test: {
+            unit : {
                 options: {
                     reporter: 'spec',
                     require: 'babel-register'
@@ -28,12 +14,6 @@ module.exports = function(grunt) {
             }
         },
 
-        open: {
-            preview: {
-                path: 'http://<%=pkg.config.host%>:<%=pkg.config.port%>/index.html',
-                app:  '<%=pkg.config.browser%>'
-            }
-        },
 
         eslint: {
             all: ['**/*.js', '!node_modules/**/*.js']
@@ -49,29 +29,11 @@ module.exports = function(grunt) {
                     'public/css/foodprint.min.css': 'public/scss/foodprint.scss'
                 }
             }
-        },
-
-        watch : {
-            options: {
-                livereload : true
-            },
-            dev : {
-                files: ['public/scss/**/*.scss'],
-                tasks: ['sass:compile']
-            }
         }
-
     });
-
-
 
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-open');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-mocha-test');
-
-    grunt.registerTask('dev', ['connect:preview', 'open:preview', 'watch:dev']);
 };
 
