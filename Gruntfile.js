7c0bbb386e7ae7ef860f032a832407dd1df2a110
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-04-21 22:12:24 +0200

    reset video files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,7 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        eslint: {
-            all: ['**/*.js', '!node_modules/**/*.js']
-        }
-    });
 
-grunt.loadNpmTasks('grunt-eslint');
+    });
 };
 
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+ app.use(express.static('public'));
 
 app.get('/hello',  (req, res) => res.send({ hello : 'wolrd'}));
 
