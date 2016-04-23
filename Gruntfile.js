f44e220b5376b6db8d0bcad654a2f1259bc3f6bb
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-04-23 23:17:44 +0200

    simplify dependencies for simplier explanations

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,36 +12,6 @@ module.exports = function(grunt) {
                 }
             }
         },
-        browserify: {
-            bundle: {
-                files: {
-                    'public/js/bundle.min.js': ['public/js/src/main.js']
-                },
-                options: {
-                    transform: [
-                        ['babelify', {
-                            'presets': ['es2015']
-                        }]
-                    ],
-                    browserifyOptions: {
-                        debug: true
-                    }
-                }
-            }
-        },
-        connect: {
-            preview: {
-                options: {
-                    hostname: '127.0.0.1',
-                    port: '4321',
-                    base: 'public'
-                }
-            }
-        }
     });
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-
-    grunt.registerTask('preview', 'Preview the application', ['browserify:bundle', 'sass:compile', 'connect:preview:keepalive']);
 };
