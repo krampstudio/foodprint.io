165ce1bb9e679af368cf11cc61b073874a63110f
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-04-30 15:31:19 +0200

    remove the clean plugin load

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-clean');
 };
 
